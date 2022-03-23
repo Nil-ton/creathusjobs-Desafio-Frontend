8fd84e980ef0b9b7f599924d5b8b9f1db41ecfdc
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2022-03-22 23:34:26 -0300

    style da aplicação foi construida

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./styles/globalStyle"
+
 function App() {
 
   return (
     <div className="App">
-      
+      <GlobalStyle />
     </div>
   )
 }
---
+++ src/styles/theme/index.tsx
@@ -0,0 +1,21 @@
+export const theme = {
+    color: {
+        font: {
+            primary: '#464646',
+            secondary: '#004098',
+            tertiary: '#A3A3A3'
+        },
+        button: {
+            primary: '#00B9FF'
+        },
+        icon: {
+            primary: '#00B9FF'
+        },
+        warning: {
+            primary: '#FFEBA3'
+        }
+    },
+    fontFamily: {
+        primary: 'Arial, Helvetica, sans-serif'
+    }
+}
