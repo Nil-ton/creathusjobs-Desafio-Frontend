9dee879031ff65ca2d7c9eaa89a88c2b6cd65a14
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2022-03-22 23:51:15 -0300

    install jest

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { WrapperHeader } from "./style";
+
+export function Header() {
+    return (
+        <WrapperHeader>
+            <div className="logo">
+                <img src="../../assets/logo.png" alt="" />
+            </div>
+        </WrapperHeader>
+    )
+}
---
+++ src/components/Header/style.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const WrapperHeader = styled.div`
+    width: 100%;
+    height: 64px;
+    display: flex;
+`
