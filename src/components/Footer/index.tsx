3fbedbce0a2fea42e84896a92fad85cb42d662e8
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2022-03-24 20:54:48 -0300

    box-shadow no footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,15 +7,17 @@ import { WrapperFooter } from './style'
 export function Footer() {
     return (
         <WrapperFooter>
-            <div className="logo" data-testid = 'logo'>
-                <img src={LogoImg} alt="imagem de uma caixa com as cores azul e laranja" />
-                <h2>OCEAN</h2>
-            </div>
-            <div className='lorem'>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, nisi, optio nesciunt dolores illo vel, sint fuga doloremque eum nobis accusantium placeat? Eos, aperiam vitae officia molestiae quis optio blanditiis.</p>
-            </div>
-            <div className='copyright'>
-                <p>Copyright © Samsung Electronics 2022. All Right Reserved</p>
+            <div className='conteiner'>
+                <div className="logo" data-testid='logo'>
+                    <img src={LogoImg} alt="imagem de uma caixa com as cores azul e laranja" />
+                    <h2>OCEAN</h2>
+                </div>
+                <div className='lorem'>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, nisi, optio nesciunt dolores illo vel, sint fuga doloremque eum nobis accusantium placeat? Eos, aperiam vitae officia molestiae quis optio blanditiis.</p>
+                </div>
+                <div className='copyright'>
+                    <p>Copyright © Samsung Electronics 2022. All Right Reserved</p>
+                </div>
             </div>
         </WrapperFooter>
     )
--- src/components/Footer/style.tsx
+++ src/components/Footer/style.tsx
@@ -3,14 +3,22 @@ import { theme } from "../../styles/theme";
 
 export const WrapperFooter = styled.div`
     width: 100%;
-    max-width: 1192px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    margin-top: 55px;
-    gap: 38px;
-    margin: 30px auto;
+    background: rgba(236, 236, 236, 1);
+    
+    
+    
+    .conteiner {
+        max-width: 1192px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        margin-top: 55px;
+        gap: 38px;
+        margin: 0 auto;
+        padding: 30px 0;
+    }
+
 
     .logo {
         display: flex;
@@ -33,6 +41,4 @@ export const WrapperFooter = styled.div`
         color: ${theme.color.font.tertiary};
         font-family: ${theme.fontFamily.primary};
     }
-
-
 `
